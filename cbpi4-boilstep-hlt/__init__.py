6f575aab112114b16cb812acfe41ac08b2376c83
avollkopf <alexander.vollkopf@gmail.com>
2024-12-17 07:19:12 +0100

    add mashstep for hlt -> second kettle in auto

--- cbpi4-boilstep-hlt/__init__.py
+++ cbpi4-boilstep-hlt/__init__.py
@@ -17,6 +17,121 @@ from cbpi.api.base import CBPiBase
 import numpy as np
 import warnings
 
+
+@parameters([Property.Number(label="Timer", description="Time in Minutes", configurable=True), 
+             Property.Number(label="Temp", configurable=True),
+             Property.Sensor(label="Sensor", description="Temp Sensor Mash Kettle"),
+             Property.Kettle(label="Kettle", description="Mash Kettle"),
+             Property.Sensor(label="Sensor2", description="Temp Sensor Sparge Water Kettle"),
+            Property.Number(label="Temp2", description="Target Temp for Sparge Water Kettle", configurable=True),
+             Property.Kettle(label="Kettle2", description="Sparge Water Kettle"),
+             Property.Select(label="AutoMode",options=["Yes","No"], description="Switch Kettlelogic automatically on and off -> Yes")])
+class MashStepHLT(CBPiStep):
+
+    @action("Start Timer", [])
+    async def start_timer(self):
+        if self.timer.is_running is not True:
+            self.cbpi.notify(self.name, 'Timer started', NotificationType.INFO)
+            self.timer.start()
+            self.timer.is_running = True
+        else:
+            self.cbpi.notify(self.name, 'Timer is already running', NotificationType.WARNING)
+
+    @action("Add 5 Minutes to Timer", [])
+    async def add_timer(self):
+        if self.timer.is_running == True:
+            self.cbpi.notify(self.name, '5 Minutes added', NotificationType.INFO)
+            await self.timer.add(300)       
+        else:
+            self.cbpi.notify(self.name, 'Timer must be running to add time', NotificationType.WARNING)
+
+
+    async def on_timer_done(self,timer):
+        self.summary = ""
+        self.kettle.target_temp = 0
+        self.spargekettle.target_temp = 0
+        if self.AutoMode == True:
+            await self.setAutoMode(False)
+        self.cbpi.notify(self.name, 'Step finished', NotificationType.SUCCESS)
+       
+        await self.next()
+
+    async def on_timer_update(self,timer, seconds):
+        self.summary = Timer.format_time(seconds)
+        await self.push_update()
+
+    async def on_start(self):
+        self.AutoMode = True if self.props.get("AutoMode", "No") == "Yes" else False
+        
+        self.kettle=self.get_kettle(self.props.Kettle)
+        if self.kettle is not None:
+            self.kettle.target_temp = int(self.props.get("Temp", 0))
+        
+        self.spargekettle=self.get_kettle(self.props.get("Kettle2", None))
+        if self.spargekettle is not None:
+            self.spargekettle.target_temp = float(self.props.get("Temp2", 0))
+
+
+        if self.AutoMode == True:
+            await self.setAutoMode(True)
+        await self.push_update()
+
+        if self.cbpi.kettle is not None and self.timer is None:
+            self.timer = Timer(int(self.props.get("Timer",0)) *60 ,on_update=self.on_timer_update, on_done=self.on_timer_done)
+        elif self.cbpi.kettle is not None:
+            try:
+                if self.timer.is_running == True:
+                    self.timer.start()
+            except:
+                pass
+
+        self.summary = "Waiting for Target Temp"
+        await self.push_update()
+
+    async def on_stop(self):
+        await self.timer.stop()
+        self.summary = ""
+        if self.AutoMode == True:
+            await self.setAutoMode(False)
+        await self.push_update()
+
+    async def reset(self):
+        self.timer = Timer(int(self.props.get("Timer",0)) *60 ,on_update=self.on_timer_update, on_done=self.on_timer_done)
+
+    async def run(self):
+        while self.running == True:
+            await asyncio.sleep(1)
+            sensor_value = self.get_sensor_value(self.props.get("Sensor", None)).get("value")
+            if sensor_value >= int(self.props.get("Temp",0)) and self.timer.is_running is not True:
+                self.timer.start()
+                self.timer.is_running = True
+                estimated_completion_time = datetime.fromtimestamp(time.time()+ (int(self.props.get("Timer",0)))*60)
+                self.cbpi.notify(self.name, 'Timer started. Estimated completion: {}'.format(estimated_completion_time.strftime("%H:%M")), NotificationType.INFO)
+        return StepResult.DONE
+
+    async def setAutoMode(self, auto_state):
+        try:
+            if (self.kettle.instance is None or self.kettle.instance.state == False) and (auto_state is True):
+                await self.cbpi.kettle.toggle(self.kettle.id)
+            elif (self.kettle.instance.state == True) and (auto_state is False):
+                await self.cbpi.kettle.stop(self.kettle.id)
+            await self.push_update()
+
+        except Exception as e:
+            logging.error("Failed to switch on KettleLogic {} {}".format(self.kettle.id, e))
+
+        try:
+            if (self.spargekettle.instance is None or self.spargekettle.instance.state == False) and (auto_state is True):
+                await self.cbpi.kettle.toggle(self.spargekettle.id)
+            elif (self.spargekettle.instance.state == True) and (auto_state is False):
+                await self.cbpi.kettle.stop(self.spargekettle.id)
+            await self.push_update()
+
+        except Exception as e:
+            logging.error("Failed to switch on KettleLogic {} {}".format(self.spargekettle.id, e))
+
+
+
 @parameters([Property.Number(label="Timer", description="Time in Minutes", configurable=True), 
              Property.Number(label="Temp", description="Boil temperature", configurable=True),
              Property.Sensor(label="Sensor",description="Boil Kettle Temperature Sensor"),
@@ -204,4 +319,5 @@ def setup(cbpi):
     '''
 
     cbpi.plugin.register("BoilStep-HLT", BoilStepHLT)
+    cbpi.plugin.register("MashStep-HLT", MashStepHLT)
 
